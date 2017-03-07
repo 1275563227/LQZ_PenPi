b83f9e5646b00935636f2c9218b7839ae0780ff6
毒豆芽 <1275563227@qq.com>
2017-03-07 21:11:29 +0800

    0307-2111版本
    完成了设置的推送关闭

--- PenPi/app/src/main/java/gdin/com/penpi/activity/SettingActivity.java
+++ PenPi/app/src/main/java/gdin/com/penpi/activity/SettingActivity.java
@@ -2,9 +2,13 @@ package gdin.com.penpi.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
+import cn.jpush.android.api.JPushInterface;
 import gdin.com.penpi.R;
 
 /**
@@ -21,6 +25,17 @@ public class SettingActivity extends AppCompatActivity {
         Toolbar mToolbar = (Toolbar) findViewById(R.id.setting_toolbar);
         mToolbar.setTitle("系统设置");
         setToolbar(mToolbar);
+
+        SwitchCompat isPush = (SwitchCompat) findViewById(R.id.setting_switch);
+        isPush.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    JPushInterface.stopPush(SettingActivity.this.getApplicationContext());
+                    Toast.makeText(SettingActivity.this, "关闭了推送通知", Toast.LENGTH_SHORT).show();
+                } else JPushInterface.init(SettingActivity.this.getApplicationContext());
+            }
+        });
     }
 
     public void setToolbar(Toolbar toolbar) {
